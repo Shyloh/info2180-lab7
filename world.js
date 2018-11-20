b2e8415dd28c6da7fe83bc995ba7fea985396c1b
Shiloh Waite <shiloh.waite@mymona.uwi.edu>
2018-11-20 03:27:51 +0000

    Exercise 4

--- world.js
+++ world.js
@@ -1,29 +1,45 @@
 window.onload= function(){
     
-    let coun = document.getElementById("country");
-    let look = document.getElementById("lookup");
-    let result= document.getElementById("result");
+    let url = 'https://lab-7-shyloh.c9users.io/world.php/world.php?all';
     let search = document.getElementById('Search');
+    let check = document.getElementById('checkbox');
+    check.checked = false;
     
-    look.addEventListener("click", fun);
     search.addEventListener("click", laugh);
     
     function laugh(){
-        let httpR = new XMLHttpRequest();
-        let url = "https://lab-7-shyloh.c9users.io/world.php/world.php?country=Jamaica";
-        httpR.onreadystatechange = function(){
-            if (httpR.readyState===4 && httpR.status===200){
-                let response =(httpR.responseText);
-                result.innerHTML = response;
-                alert (response);
-                }
-            };
-            httpR.open("GET", url);
-            httpR.send();
+        let name=document.getElementById('country').value;
+        check.checked;
+        let xhttp=new XMLHttpRequest();
+        xhttp.onreadystatechange=function(){
+            if(xhttp.readyState=== 4 && xhttp.status===200){
+                let info=document.getElementById('result');
+                let response = xhttp.response;
+                info.innerHTML=response;
+                document.getElementById('checkbox').checked=false;
+            }
+        } ;
+        xhttp.open('GET','world.php?country='+name+'&checkbox='+check,true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send();
     }
-    function fun(){
+    check.addEventListener("click", function(){
+        check.checked = true;
+        let name=document.getElementById('country').value;
+        check.checked;
+        let xhttp=new XMLHttpRequest();
+        xhttp.onreadystatechange=function(){
+            if(xhttp.readyState=== 4 && xhttp.status===200){
+                let info=document.getElementById('result');
+                let response = xhttp.response;
+                info.innerHTML=response;
+                document.getElementById('checkbox').checked=false;
+            }
+        } ;
+        xhttp.open('GET',url+name+'&checkbox='+check,true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send();
         
-    }
+    });
     
-
-};   
+};
